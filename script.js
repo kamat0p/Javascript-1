4ad5207410c5b45f438475cf2e3eb96a7945c6b5
kamrom <kamrom@gm.ru>
2023-06-22 19:54:25 +0500

    javascript

---
+++ script.js
@@ -0,0 +1,21 @@
+var userName = prompt('Введите свое имя')
+console.log(userName);
+
+var age = prompt('Введите свой возраст')
+console.log(age);
+
+var pr1 = +prompt('6 + 6 = ?')
+console.log('Пример 1: 6 + 6 = ' + (6 + 6) + " (Ваш ответ " + pr1 + " )")
+
+var pr2 = +prompt('30 - 5 = ?')
+console.log('Пример 2: 30 - 5 = ' + (30 - 5) + " (Ваш ответ " + pr2 + " )")
+
+var pr3 = +prompt('6 * 6 = ?')
+console.log('Пример 3: 6 * 6 = ' + (6 *  6) + " (Ваш ответ " + pr3 + " )")
+
+var pr4 = +prompt('65 / 5 = ?')
+console.log('Пример 4: 65 / 5 = ' + (65 / 5) + " (Ваш ответ " + pr4 + " )") 
+
+var pr5 = +prompt('65 / 55 = ?')
+console.log('Пример 4: 65 / 55 = ' + (65 / 55) + " (Ваш ответ " + pr5 + " )") 
+alert('Если сделал ошибку то ты не натурал иди проверять в консоль')
